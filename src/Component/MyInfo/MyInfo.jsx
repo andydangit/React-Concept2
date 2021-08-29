1e2a636d817b9ff0b4be0cb140fe96ef232d0881
pandoragear <andy.dang.it@gmail.com>
2021-08-28 19:25:42 -0500

    add inital files to MyInfo

---
+++ src/Component/MyInfo/MyInfo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MyInfo() {
+  return (
+    <div>
+      <h1> Andy Dang </h1>
+      <p>
+        I'm going to be a <strong>S</strong>oftware <strong>D</strong>eveloper
+      </p>
+      <ul>
+        <li> FrontEnd</li>
+        <li> BackEnd</li>
+        <li> Python</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyInfo;

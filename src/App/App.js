08e366d671ede9335a35eedb5df5bae98a664add
pandoragear <andy.dang.it@gmail.com>
2021-08-28 19:22:03 -0500

    Start refresh

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import PersonList from "../Component/PersonList";
-import PersonInput from "../Component/PersonInput";
+
 
 function App() {
   return (
     <div className="App">
-      <PersonInput />
-      <PersonList />
+
     </div>
   );
 }

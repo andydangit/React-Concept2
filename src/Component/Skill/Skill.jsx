e2e4e324a573320fbc292a484f74c2cc406d2218
pandoragear <andy.dang.it@gmail.com>
2021-08-28 19:38:37 -0500

    add langauge skill

---
+++ src/Component/Skill/Skill.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Skill() {
+  return (
+    <div>
+        <h4>FrontEnd </h4>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li> React </li>
+      </ul>
+<h4> BackEnd</h4>
+      <ul>
+        <li> Node.js </li>
+        <li> MySQL </li>
+        <li> Python </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Skill;

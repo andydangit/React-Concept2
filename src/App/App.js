ef4fdeb6fa1a3b337e9ee39c88aeadd2ff6d94c2
pandoragear <andy.dang.it@gmail.com>
2021-08-28 19:49:00 -0500

    add Footer

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import MyInfo from "../Component/MyInfo/MyInfo";
+import Footer from "../Component/Footer/Footer";
 
 
 function App() {
   return (
     <div className="App">
       <MyInfo /> 
+      <Footer />
     </div>
   );
 }

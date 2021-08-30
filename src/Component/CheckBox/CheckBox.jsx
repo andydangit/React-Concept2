e0a53d9d8b8c4aef3ae635d074c8e3bfab4cc759
pandoragear <andy.dang.it@gmail.com>
2021-08-29 23:22:47 -0500

    add lable and input checkbox

--- src/Component/CheckBox/CheckBox.jsx
+++ src/Component/CheckBox/CheckBox.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-function CheckBox() {
+function Checkbox() { 
     return (
-        <div>
-            
-        </div>
+        <div> 
+        <p> I will dedicated the time to study </p> 
+        <form>
+        <input type="checkbox" /> 
+        <label> Studying React </label> 
+        <br/>
+        <input type="checkbox" /> 
+        <label> remember the syntex  </label> 
+        </form> 
+        </div> 
     )
 }
 
-export default CheckBox
+export default Checkbox;

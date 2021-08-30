8080ef2b6c358ada7606a9c6dd27374fe69cd3fc
pandoragear <andy.dang.it@gmail.com>
2021-08-29 23:25:33 -0500

    Add Checkbox to app

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import MyInfo from "../Component/MyInfo/MyInfo";
 import Footer from "../Component/Footer/Footer";
+import Checkbox from "../Component/CheckBox/CheckBox";
 
 
 function App() {
   return (
     <div className="App">
       <MyInfo /> 
+      <Checkbox />
       <Footer />
     </div>
   );

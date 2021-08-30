58c6a3f637219e1ef1f168c705d9e5a616b77312
pandoragear <andy.dang.it@gmail.com>
2021-08-29 23:55:42 -0500

    add a date time

--- src/Component/MyInfo/MyInfo.jsx
+++ src/Component/MyInfo/MyInfo.jsx
@@ -3,12 +3,16 @@ import '../MyInfo/MyInfo.css'
 import Skill from "../Skill/Skill";
 
 function MyInfo() {
+
+    const date = new Date()
+
   return (
     <div>
       <h1> Andy Dang </h1>
       <p>
         I'm going to be a <strong>S</strong>oftware <strong>D</strong>eveloper
       </p>
+      <p> it is about {date.getHours() % 12} o'clock; get to practice coding</p>
       
       <Skill /> 
     </div>

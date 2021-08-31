d7fa10b5f760c9aeb3d518c908dc2f3976a12db2
pandoragear <andy.dang.it@gmail.com>
2021-08-30 20:10:21 -0500

    add informaiton to jokes

--- src/App/App.js
+++ src/App/App.js
@@ -2,13 +2,27 @@ import "./App.css";
 import MyInfo from "../Component/MyInfo/MyInfo";
 import Footer from "../Component/Footer/Footer";
 import Checkbox from "../Component/CheckBox/CheckBox";
-
+import Joke from "../Component/Joke/Joke";
 
 function App() {
   return (
     <div className="App">
-      <MyInfo /> 
+      <MyInfo />
       <Checkbox />
+      <Joke jokes={{ punchline: "None, becuase it's a hardware problem" }} />
+      
+      <Joke
+        jokes={{
+          question: "What's the best thing about Switzerland?",
+          punchline: "I don't know, but the flag is a big plus!",
+        }}
+      />
+      <Joke
+        jokes={{
+          question: "Hear about the new restaurant called Karma?",
+          punchline: "THere's no menu: you get what you deserve",
+        }}
+      />
       <Footer />
     </div>
   );
--- src/Component/Joke/Joke.jsx
+++ src/Component/Joke/Joke.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Joke() { 
     return(
         <> 
-
+        
         </> 
     )
 }

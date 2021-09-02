e27fc1cf6eb03da3f03c3f0d8580f345e53a32a7
pandoragear <andy.dang.it@gmail.com>
2021-09-01 23:27:37 -0500

    create test component

---
+++ src/Test/test.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Create Class Component 
+
+class test extends React.Component {
+    render() { 
+        return (
+            <> 
+            </> 
+        )
+    }
+}
+
+export default test
